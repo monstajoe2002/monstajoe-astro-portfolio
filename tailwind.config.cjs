3e6a268ba296759ff3b3ae19cd534eae0fe6da2d
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-04-25 20:27:36 +0200

    some housekeeping and design changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"blue-primary": "#1a8fdd",
-				"blue-secondary": "#1A68DD",
-				"light-blue":"#cddcff"
-			}
-		},
-		screens: {
-			'sm': '640px',
-			// => @media (min-width: 640px) { ... }
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "blue-primary": "#1a8fdd",
+        "blue-secondary": "#1A68DD",
+        "light-blue": "#cddcff",
+      },
+      animation: {
+        fade: "fadeIn 0.6s ease-in-out", //fade in class
+      },
+      keyframes: { //keyframes go here
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
 
-			'md': '768px',
-			// => @media (min-width: 768px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
 
-			'lg': '1024px',
-			// => @media (min-width: 1024px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
 
-			'xl': '1280px',
-			// => @media (min-width: 1280px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
 
-			'2xl': '1536px',
-			// => @media (min-width: 1536px) { ... }
-		}
-	},
-	plugins: [],
-}
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+};
